70c5cfa95c02060075eedcc9f682b7534e022ce8
Jose Luis Chavez del Cid <jlchavez@gmail.com>
2024-06-24 01:59:36 -0600

    Javascript location customization
    Following the pattern of other libraries from this author added support for settings the customization of the location of the javascript file.

---
+++ src/KristofferStrube.Blazor.Streams/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using KristofferStrube.Blazor.FileAPI.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+
+namespace KristofferStrube.Blazor.Streams;
+
+public static class IServiceCollectionExtensions
+{
+    public static IServiceCollection AddStreams(this IServiceCollection serviceCollection, Action<StreamsOptions>? configure)
+    {
+        serviceCollection.ConfigureStreamOptions(configure);
+        return serviceCollection;
+    }
+
+    private static void ConfigureStreamOptions(this IServiceCollection services, Action<StreamsOptions>? configure)
+    {
+        if (configure is null) return;
+
+        services.Configure(configure);
+        configure(StreamsOptions.DefaultInstance);
+    }
+}
